129f2ad6a4f921ecf9e02e408398209848c71ffc
Steve Clark <steve.a.clark48@gmail.com>
2021-08-03 11:36:29 -0500

    wrote employee class

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50, default=("temp"))
+    zip_code = models.CharField(max_length=10, null=True)
+    user = models.ForeignKey('account.User', blank=True, null=True, on_delete=models.CASCADE)
