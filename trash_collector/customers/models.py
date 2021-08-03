315d89511bb7ca423fad23cf87c1663554800d3b
codymart1986 <codymart1986@hotmail.com>
2021-08-03 09:15:48 -0700

    set up customer model

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -7,3 +7,13 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100, null=True)
+    zip_code = models.CharField(max_length=5)
+    weekly_pickup_day = models.CharField(max_length=10, null=True)
+    onetime_pickup = models.DateField(null=True)
+    start_supsension = models.DateField(null=True)
+    end_supsension = models.DateField(null=True)
+    balance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
