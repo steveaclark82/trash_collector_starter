5dc520537365c862cd82dbbd33f878fab2fd87cb
Steve Clark <steve.a.clark48@gmail.com>
2021-08-03 10:30:01 -0500

    created repo

---
+++ trash_collector/customers/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Customer
+# Create your views here.
+
+# TODO: Create a function for each path created in customers/urls.py. Each will need a template as well.
+
+
+def index(request):
+    # The following line will get the logged-in in user (if there is one) within any view function
+    user = request.user
+
+    try:
+        # This line inside the 'try' will return the customer record of the logged-in user if one exists
+        logged_in_customer = Customer.objects.get(user=user)
+    except:
+        # TODO: Redirect the user to a 'create' function to finish the registration process if no customer record found
+        pass
+
+    # It will be necessary while creating a Customer/Employee to assign request.user as the user foreign key
+
+    print(user)
+    return render(request, 'customers/index.html')
