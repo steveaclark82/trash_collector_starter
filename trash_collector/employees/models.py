0f0614296a8a00773509679fc250aa18f5bb427b
Steve Clark <steve.a.clark48@gmail.com>
2021-08-04 10:34:14 -0500

    updated class

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # TODO: Create an Employee model with properties required by the user stories
 
-class Employee(models.Model):
+class Employees(models.Model):
     name = models.CharField(max_length=50, default=("temp"))
     zip_code = models.CharField(max_length=10, null=True)
     user = models.ForeignKey('account.User', blank=True, null=True, on_delete=models.CASCADE)
