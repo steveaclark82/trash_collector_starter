2c64bad87cf7834f4fbb19d7c6cf3638cf6f6b53
Steve Clark <steve.a.clark48@gmail.com>
2021-08-04 15:34:37 -0500

    Changed urls back to previous

--- trash_collector/customers/urls.py
+++ trash_collector/customers/urls.py
@@ -7,9 +7,6 @@ from . import views
 app_name = "customers"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('registercustomer/', views.create_customer, name='registercustomer'),
-    path('setpickup/', views.weekly_pickup, name="setpickup"),
-    path('pickup_suspension/', views.suspend_pickup, name="pickup_suspension"),
-    path('account_info/', views.account_info, name="account_info"),
-    path('onetime_pickup/', views.onetime_pickup, name="onetime_pickup")
+    path('create/', views.create, name="create"),
+    path('edit/<int:option>/', views.edit, name="edit")
 ]
