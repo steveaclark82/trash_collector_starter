e356cb1a6dbb58edfc50ec5e7eac205ed0f56077
Steve Clark <steve.a.clark48@gmail.com>
2021-08-05 12:54:21 -0500

    fixed file from revert

--- trash_collector/customers/urls.py
+++ trash_collector/customers/urls.py
@@ -7,9 +7,6 @@ from . import views
 app_name = "customers"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('register_customer/', views.create_customer, name='register_customer'),
-    path('set_pickup/', views.weekly_pickup, name="set_pickup"),
-    path('pickup_suspension/', views.suspend_pickup, name="pickup_suspension"),
-    path('account_info/', views.account_info, name="account_info"),
-    path('onetime_pickup/', views.onetime_pickup, name="onetime_pickup")
+    path('create/', views.create, name="create"),
+    path('edit/<int:option>/', views.edit, name="edit")
 ]
