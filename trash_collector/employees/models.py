c610366d8efb0684c04c3daeb73daa2e4d4dc61a
Tabor <86973131+TaborMather-dev@users.noreply.github.com>
2021-08-05 19:01:59 -0700

    created and registered employee model

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+    employee_name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.Employee', blank=True, null=True, on_delete=models.CASCADE)
+    zip_code = models.CharField(length=5)
+    customer_list = models.CharField(models.list)
+
+    def __str__(self):
+        return self.employee_name
