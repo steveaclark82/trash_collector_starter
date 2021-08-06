cf4f028549057ce953a36819a4551bb62150656b
Steve Clark <steve.a.clark48@gmail.com>
2021-08-06 09:14:33 -0500

    fixed spelling

--- trash_collector/employees/admin.py
+++ trash_collector/employees/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employees
 # Register your models here.
 
 
-admin.site.register(Employee)
+admin.site.register(Employees)
