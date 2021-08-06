64ad05516290fc25285d120071d53b1fe139d33e
Steve Clark <steve.a.clark48@gmail.com>
2021-08-06 09:11:05 -0500

    fixed errors

--- trash_collector/employees/admin.py
+++ trash_collector/employees/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Employee
 # Register your models here.
 
 
 admin.site.register(Employee)
-=======
-from .models import Employees
-# Register your models here.
-admin.site.register(Employees)
->>>>>>> parent of b50c532 (init)
--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -3,20 +3,9 @@ from django.db import models
 
 # TODO: Create an Employee model with properties required by the user stories
 
-<<<<<<< HEAD
-class Employee(models.Model):
-    employee_name = models.CharField(max_length=50)
-    user = models.ForeignKey('accounts.Employee', blank=True, null=True, on_delete=models.CASCADE)
-    zip_code = models.CharField(length=5)
-    customer_list = models.CharField(models.list)
-
-    def __str__(self):
-        return self.employee_name
-=======
 class Employees(models.Model):
     name = models.CharField(max_length=50, default="")
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.PROTECT)
     zip_code = models.IntegerField(default=00000, null=True)
     todays_customers = []
 
->>>>>>> parent of b50c532 (init)
